3b4b05b9abeb62b483da365ffbb14689f0aef1ef
Tom Bremer <tom@tbremer.com>
2015-09-23 15:34:58 -0500

    update main module, update gitignore, create templates, register partials, register helpers, add in resource gathering

---
+++ TODO.md
@@ -0,0 +1,15 @@
+Todo
+---
+
+## Log whats happening.
+
+## Internal Modules
+    1. write-html
+        - use all async solutions
+        - handle markdown.render in a more legible way.
+
+## Handlebars
+1. helpers
+    -  resource
+        - allow HTTP/S resources.
+
--- bin/app.js
+++ bin/app.js
@@ -1,6 +1,26 @@
 #!/usr/bin/env node
 
-var glob = require('glob');
-var filePattern = process.argv.slice(2)[0];
+var cliOptions = require('yargs')
+  .usage('styleguider --input **/*.css [options]')
+  .alias('input', 'i')
+  .describe('input', 'file(s) (glob pattern) that should be evaulated and built into the Styleguide')
+  .alias('output', 'o')
+  .describe('output', 'Where should the output styleguide go? Defaults to current working directory /styleguide')
+  .alias('resource', 'r')
+  .describe('resource', 'additional resources to be loaded into the code example iframes')
+  //.alias('c', 'config')
+  //.alias('t', 'template')
+  .alias('help', 'h')
+  .describe('help', 'Show this Dialog')
+  .array('resource')
+  .demand('input')
+  .help('h')
+  .argv;
 
-require('../index.js')(filePattern);
+var options = {
+  input: cliOptions.input,
+  output: cliOptions.output || undefined,
+  resources: cliOptions.resource || []
+}
+
+require('../index.js')(options);
