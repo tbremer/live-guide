ea658ad6862bf3425698cf56e3c64b85a49637fa
Tom Bremer <tom@tbremer.com>
2015-09-25 16:29:39 -0500

    Adjust iframe height based on content size, add base level app script, move templates to inside write-html module

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 (function() {
-  alert('hey, I am here now');
-})();
+  window.addEventListener("message", function(event) {
+    var element = document.getElementById(event.data.id);
+    if (!element) { return false; }
 
+    element.style.width = event.data.width + 'px';
+    element.style.height = event.data.height + 'px';
+    console.log('MESSAGE RECIEVED');
+    console.log(event.data);
+  }, false)
+})();
