40a02832ddc65e30d4625534f420f6edbb57918c
Tom Bremer <tom@tbremer.com>
2015-09-29 16:43:41 -0500

    add myth and watch as dev dependencies, start building out stylesheet, tweak HTML as needed for CSS udpates

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,7 @@
     var element = document.getElementById(event.data.id);
     if (!element) { return false; }
 
-    element.style.width = event.data.width + 'px';
-    element.style.height = event.data.height + 'px';
-    console.log('MESSAGE RECIEVED');
-    console.log(event.data);
+    //element.style.width = event.data.width + 'px';
+    //element.style.height = event.data.height + 'px';
   }, false)
 })();
