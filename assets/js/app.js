548d8da114438dd5d7df4ef99037f01decc416f5
Tom Bremer <tom@tbremer.com>
2015-10-07 14:46:09 -0500

    Update iframes to account for absolutely positioned elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,8 @@
 (function() {
-  window.addEventListener("message", function(event) {
+  window.addEventListener('message', function(event) {
     var element = document.getElementById(event.data.id);
     if (!element) { return false; }
 
-    //element.style.width = event.data.width + 'px';
     element.style.height = event.data.height + 'px';
   }, false)
 })();
